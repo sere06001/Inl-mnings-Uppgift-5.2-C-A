0ce68f60e2e3471d5ea06342cfa814406ff6915a
sere06001 <sere06001@utb.vaxjo.se>
2023-11-22 16:08:28 +0100

    Add project files.

---
+++ Inlämnings Uppgift 5.2 C-A/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Uppgift5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många heltal vill du skriva in?");
+            int antalHeltal = int.Parse(Console.ReadLine());
+            int[] tal = new int[antalHeltal];
+            Console.WriteLine("Skriv in "+antalHeltal+" heltal");
+            for (int i = 0; i < antalHeltal; i++)
+            {
+                tal[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine();
+            Console.WriteLine("Här är talen du skrev in");
+            for (int i = 0; i < antalHeltal; i++)
+            {
+                Console.WriteLine("Tal "+(i+1)+": " + tal[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
